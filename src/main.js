3ba149de8ea3824221d7bfd7d80dc12581ac3c7d
Vinicius Martins <viniciusmartinsdacunha1@gmail.com>
2022-11-11 19:01:38 -0300

    thanks and contacts

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const target = document.querySelectorAll("[data-anime]")
 const animationClass = "animate";
 
 function scroll(){
-  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
 
   target.forEach(function(e){
     if(windowTop > e.offsetTop){
